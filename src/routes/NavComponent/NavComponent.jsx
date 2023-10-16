9312d069ccc2dce870f24a4eacd693e689d2dfb8
ArunJ03 <smilearun03@gmail.com>
2023-10-16 10:49:43 +0530

    Initial commit

---
+++ src/routes/NavComponent/NavComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LoginComponent from '../LoginComponent/LoginComponent'
+import SignupComponent from '../SignupComponent/SignupComponent'
+
+const NavComponent = () => {
+  return (
+    <React.Fragment>
+    <LoginComponent/>
+    <SignupComponent/>
+    </React.Fragment>
+    
+  )
+}
+
+export default NavComponent
